6ef4926d043c0e66ed5149d5bdfb256447400b41
Emilio_Ballesta <efballes@uc.cl>
2020-05-21 20:42:10 -0400

    full client flow

--- src/views_client_petition/Second_step.js
+++ src/views_client_petition/Second_step.js
@@ -3,10 +3,44 @@ import React, { Component } from 'react';
 class QuestType extends Component {
     render() {
         return (
-            <form>
+            <div>
+
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <div className="row  col-md-3 offset-sm-1">
+                    <a className="navbar-brand" href="#">
+                        S H I P B A C K
+                    </a>
+                </div>
+                <div className="row  col-sm-2 offset-sm-5">
+                    <ul className="navbar-nav pl-5">
+                        <li className="nav-item active">
+                            <a className="nav-link offset-sm-5" href="#">
+                                Account
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="pt-5"></div>
+            <div class="pt-5"></div>
+            <div class="container-lg mx-auto pr-3 mr-3 mt-3">
+                <button type="button" class="btn btn-light ml-5">1</button>
+                <span class="badge badge-pill pr-0 pl-0">———————</span>
+                <button type="button" class="btn btn-dark">2</button>
+                <span class="badge badge-pill pr-0 pl-0">———————</span>
+                <button type="button" class="btn btn-light">3</button>
+                <span class="badge badge-pill pr-0 pl-0">———————</span>
+                <button type="button" class="btn btn-light">4</button>
+                <span class="badge badge-pill pr-0 pl-0">———————</span>
+                <button type="button" class="btn btn-light">5</button>
+            </div>
+
+
+            <form class="container-md">
+                <div class="pb-5"></div>
                 <legend class="col-form-label font-weight-bold">Detalles del pedido</legend>
                 <legend class="col-form-label">Seleccione los productos que desea cambiar o devolver. Debe elegir al menos 1.</legend>
-                <div class="pb-3"></div>
+                <div class="pb-5"></div>
                 <table class="table pt-3">
                     <tr>
                         <th scope="col">Número</th>
@@ -24,18 +58,19 @@ class QuestType extends Component {
                     <div class="form-check">
                         <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked />
                         <label class="form-check-label" for="gridRadios1" />Cambiar producto
-                    </div>
+                </div>
                     <div class="form-check">
                         <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked />
                         <label class="form-check-label" for="gridRadios1" />Devolver producto
-                    </div>
+                </div>
                 </fieldset>
                 <div class="pb-3"></div>
-                <a href="http://web.whatsapp.com" type="button" class="btn btn-success"><i class="fa fa-whatsapp"></i> ¡Contáctanos por What's App!</a>
+                <a href="https://api.whatsapp.com/send?phone=+56993232898&text=&source=&data=&app_absent=" type="button" class="btn btn-success"><i class="fa fa-whatsapp"></i> ¡Contáctanos por What's App!</a>
                 <div class="pb-3"></div>
-                <a href="petitions_1" type="button" class="btn btn-primary mr-1">Anterior</a>
-                <a href="petitions_2" type="button" class="btn btn-primary">Siguiente</a>
-            </form>            
+                <a href="petitions_1" type="button" class="btn btn-secondary mr-1">Anterior</a>
+                <a href="petitions_3" type="button" class="btn btn-dark">Siguiente</a>
+            </form>
+        </div>          
         );
     }
 }
